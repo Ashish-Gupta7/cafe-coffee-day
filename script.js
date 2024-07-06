9927bd27603bfc33e75e31c8806e6e79439a33e5
Ashish-Gupta7 <ash300mr@gmail.com>
2024-07-06 11:11:00 +0530

    completed

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
